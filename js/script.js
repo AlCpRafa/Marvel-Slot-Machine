45138cb54e8e9282701a74556f78699873a1b6b2
AlCpRafa <ralonsocepeda@gmail.com>
2022-11-09 12:58:14 +0100

    commit inicial

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const slot__containers = document.querySelector(".slot__container");
+const slot_1 = document.querySelector("#slot1");
+const slot_2 = document.querySelector("#slot2");
+const slot_3 = document.querySelector("#slot3");
+
+let heroes = ["america", "avengers", "hulk", "ironman", "thor"];
