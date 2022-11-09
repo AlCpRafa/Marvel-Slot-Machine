3a464b66d3de6502adcb898b1b7444d7dd49aa35
AlCpRafa <ralonsocepeda@gmail.com>
2022-11-09 13:00:24 +0100

    se crean todos los identificadores en js, la estructura html y el css es provisional, de prueba

--- js/script.js
+++ js/script.js
@@ -2,5 +2,6 @@ const slot__containers = document.querySelector(".slot__container");
 const slot_1 = document.querySelector("#slot1");
 const slot_2 = document.querySelector("#slot2");
 const slot_3 = document.querySelector("#slot3");
+const playbtn = document.querySelector("#playbtn");
 
 let heroes = ["america", "avengers", "hulk", "ironman", "thor"];
